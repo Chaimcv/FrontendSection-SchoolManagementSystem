0332cf0c0cffd05e3f02c9980e54d47b78cd5583
Chaimcv <chaithanyamcv@gmail.com>
2026-03-05 23:22:30 +0530

    updates

--- src/pages/views/Student/ViewStudentDetails.jsx
+++ src/pages/views/Student/ViewStudentDetails.jsx
@@ -15,7 +15,7 @@ const ViewStudentDetails = () => {
    
   console.log(getStudentDetails,"get Student details");
    const EditStudentForm=()=>{
-    //incomp
+    //incomplete.................
    }
   // const BackToStudentListing=()=>{
   // navigate("/allStudentslisted/");
