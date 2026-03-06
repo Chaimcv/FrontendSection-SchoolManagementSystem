0f91966d322b834859744ec4aa95837b5661ecef
Chaimcv <chaithanyamcv@gmail.com>
2026-03-06 23:17:52 +0530

    updates

--- src/pages/views/Student/EditStudentForm.jsx
+++ src/pages/views/Student/EditStudentForm.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const EditStudentForm = () => {
 const dispatch=useDispatch();
 const studentDetails=useSelector((state)=>state.student.oneStudentDetailsData)
 console.log(studentDetails,"student details");
+const[studentForm,setStudentForm]=useState();
 useEffect(()=>{
 
 },[]);
