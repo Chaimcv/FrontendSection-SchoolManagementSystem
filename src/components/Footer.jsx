72a244484b11da74bf555bcaaf57c6f1700310c9
Chaithanya <chaithanyamcv@gmail.com>
2026-04-18 22:26:52 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             <li>Fee Collection</li>
             <li>Cyber Security Awareness </li>
             <li>Right to Information</li>
-            <></>
+            <>additions</>
           </ul>
          </div>
           <div className='w-[50%]'>
