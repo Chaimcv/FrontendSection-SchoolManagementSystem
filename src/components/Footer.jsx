5233b948edc167ad7be734f22b8934d044b99573
Chaimcv <chaithanyamcv@gmail.com>
2026-05-05 22:56:29 +0530

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
       <div className='flex' >
          <div className='m-5 w-[50%]'>
           <ul className='font-semibold list-disc ml-32'>
-            {/* <li>Online Admission</li>
+            <li>Online Admission</li>
             <li>Fee Collection</li>
             <li>Cyber Security Awareness </li>
-            <li>Right to Information</li> */}
+            <li>Right to Information</li>
             
           </ul>
          </div>
