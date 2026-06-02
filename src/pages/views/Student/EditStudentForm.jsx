aecdf1ad1a392765f385fd57606c70fb4a3716c4
Chaimcv <chaithanyamcv@gmail.com>
2026-06-02 16:21:04 +0530

    updates

--- src/pages/views/Student/EditStudentForm.jsx
+++ src/pages/views/Student/EditStudentForm.jsx
@@ -34,10 +34,9 @@ if(studentDetails){
       Guardian_Phonenumber:studentDetails.Guardian_Phonenumber||"",
       Address:studentDetails.Address||"",
       Pincode:studentDetails.Pincode
-    });
-  
+    }); 
 }
-},[]);
+},[studentDetails]);
 const edited=(id)=>{
  console.log(id,"student edit id");
  dispatch(postEditedStudentDetails({}))
@@ -46,15 +45,19 @@ const edited=(id)=>{
     <div>
       <form onSubmit={edited(studentDetails?._id)}>
         <div className='border border-black flex flex-col m-5 p-5 space-y-5'>
-           <h2>Name : </h2>
-           <h2>Age : </h2>
-           <h2>Gender : </h2>
-           <h2>Standard : </h2>
-           <h2>Division : </h2>
-           <h2>Guardian Name : </h2>
-           <h2>Guardian Phonenumber : </h2>
-           <h2>Address : </h2>
-           <h2>Pincode : </h2>
+           <h2>Name : <input type='text' name='Name' value={studentForm.Name}></input></h2>
+           <h2>Age : <input type='number' name='Age' value={studentForm.Age}></input></h2>
+           <h2>Gender : <select>
+            <option value="">{studentForm.Gender}</option>
+             <option value="Female"></option>
+              <option value="Male"></option>
+            </select></h2>
+           <h2>Standard : <input type='number' name='Standard' value={studentForm.Standard}></input></h2>
+           <h2>Division :<input type='text' name='Division' value={studentForm.Division}></input> </h2>
+           <h2>Guardian Name : <input type='text' name='Guardian' value={studentForm.Guardian_Name}></input></h2>
+           <h2>Guardian Phonenumber :<input type='number' name='Guardian number' value={studentForm.Guardian_Phonenumber}></input> </h2>
+           <h2>Address :<input type='text' name='Address' value={studentForm.Address}></input> </h2>
+           <h2>Pincode : <input type='number' name='pincode' value={studentForm.Pincode}></input></h2>
         </div>
       </form> 
     </div>
