5650e1616158c4ad4add3c58040801ff3fab440b
Chaimcv <chaithanyamcv@gmail.com>
2026-06-04 23:37:39 +0530

    design

--- src/pages/views/Parent/ParentsList.jsx
+++ src/pages/views/Parent/ParentsList.jsx
@@ -45,8 +45,8 @@ const ParentsList = () => {
          <h3>Add Parent  <button className='rounded-md bg-amber-500 px-7 ml-14 mt-1' onClick={AddParent}>+</button></h3></div> */}
       <div>
         <h3 className='font-bold p-3 text-amber-800 text-2xl'>Parents</h3>
-          <table className='auto p-2 mx-[15%] shadow-md rounded-md mb-[50%]'>
-            <thead className='bg-white text-amber-700 border rounded-lg'>
+          <table className='auto p-2 mx-[15%] shadow-md rounded-2xl mb-[50%]'>
+            <thead className='bg-white text-amber-700 border rounded-2xl'>
               <tr className='p-5'>
                 <th className='p-5'> NAME </th>
                 <th className='p-5'> EMAIL </th>
