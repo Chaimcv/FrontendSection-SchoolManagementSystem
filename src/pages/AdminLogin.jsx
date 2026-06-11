95fa73aee233b569ed49371c917f4d9e5708c20d
Chaimcv <chaithanyamcv@gmail.com>
2026-06-11 21:44:56 +0530

    responsiveness done

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -57,8 +57,8 @@ const AdminLogin = () => {
     {/* small screen */}
     <div className='flex lg:hidden'>
 
-         <div className='flex rounded-lg w-screen m-2 h-screen'>
-           <div className=' bg-amber-400  p-1 w-[90%] space-y-5'>
+          <div className='flex rounded-lg  w-screen  h-screen '>
+           <div className=' bg-amber-400  p-1 w-[100%] space-y-4 pb-[5%] flex-row h-fit'>
               <h2>ADMIN LOGIN</h2>
               <br />
               <label>Email <input className='p-1 mx-1 mt-20 rounded-lg w-[85%] ' type='email' placeholder='Enter  registered email' value={adminEmail} onChange={(e)=>setAdminemail(e.target.value)} /></label><br />
