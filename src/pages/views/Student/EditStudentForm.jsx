caf5a1dc22a79eb9119221415748ca1eeb1005cf
Chaimcv <chaithanyamcv@gmail.com>
2026-06-19 17:55:22 +0530

    edit function

--- src/pages/views/Student/EditStudentForm.jsx
+++ src/pages/views/Student/EditStudentForm.jsx
@@ -58,19 +58,19 @@ const BackToStudentDetails=()=>{
     <div>
       <form onSubmit={edited(studentDetails?._id)}>
         <div className='border border-black flex flex-col m-5 p-5 space-y-5'>
-           <h2>Name : <input type='text' name='Name' value={studentForm.Name}  onchange={handleChange}></input></h2>
-           <h2>Age : <input type='number' name='Age' value={studentForm.Age}  onchange={handleChange}></input></h2>
-            <h2>Gender : <select  onchange={handleChange}>
+           <h2>Name : <input type='text' name='Name' value={studentForm.Name}  onChange={handleChange}></input></h2>
+           <h2>Age : <input type='number' name='Age' value={studentForm.Age}  onChange={handleChange}></input></h2>
+            <h2>Gender : <select  onChange={handleChange}>
               <option value="">{studentForm.Gender}</option>
                <option value="Female"></option>
                <option value="Male"></option>
             </select></h2>
-           <h2>Standard : <input type='number' name='Standard' value={studentForm.Standard}  onchange={handleChange}></input></h2>
-           <h2>Division :<input type='text' name='Division' value={studentForm.Division}  onchange={handleChange}></input> </h2>
-           <h2>Guardian Name : <input type='text' name='Guardian' value={studentForm.Guardian_Name}  onchange={handleChange}></input></h2>
+           <h2>Standard : <input type='number' name='Standard' value={studentForm.Standard}  onChange={handleChange}></input></h2>
+           <h2>Division :<input type='text' name='Division' value={studentForm.Division}  onChange={handleChange}></input> </h2>
+           <h2>Guardian Name : <input type='text' name='Guardian' value={studentForm.Guardian_Name}  onChange={handleChange}></input></h2>
            <h2>Guardian Phonenumber :<input type='number' name='Guardian number' value={studentForm.Guardian_Phonenumber}  onchange={handleChange}></input> </h2>
-           <h2>Address :<input type='text' name='Address' value={studentForm.Address}  onchange={handleChange}></input> </h2>
-           <h2>Pincode : <input type='number' name='pincode' value={studentForm.Pincode}  onchange={handleChange}></input></h2>
+           <h2>Address :<input type='text' name='Address' value={studentForm.Address}  onChange={handleChange}></input> </h2>
+           <h2>Pincode : <input type='number' name='pincode' value={studentForm.Pincode}  onChange={handleChange}></input></h2>
            <button type='submit'>Submit</button>
            <button onClick={BackToStudentDetails}>Back</button>
         </div>
