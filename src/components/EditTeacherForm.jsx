cadb32517bb2a0738271a358e953d7698579f471
Chaimcv <chaithanyamcv@gmail.com>
2026-07-03 22:51:18 +0530

    updates

--- src/components/EditTeacherForm.jsx
+++ src/components/EditTeacherForm.jsx
@@ -49,9 +49,15 @@ const handleChange=(e)=>{
         <form onSubmit={EditTeacher(TeacherDataFromDatabase?._id)}>
             {TeacherDataFromDatabase ? (
         <div className='border border-black flex flex-col m-5 p-5 space-y-5 bg-amber-100'>
-            <label>NAME: <input type='text' name="name" value={formData.name}   onChange={handleChange} /></label>
-            <label>PINCODE: <input type='text' name="pin" value={formData.pin}  onChange={handleChange} /></label>
-            <label>CITY: <input type='text'   name="city" value={formData.city}  onChange={handleChange} /></label>
+            <label>NAME: 
+              <input type='text' name="name" value={formData.name}   onChange={handleChange} />
+              </label>
+            <label>PINCODE:
+               <input type='text' name="pin" value={formData.pin}  onChange={handleChange} />
+               </label>
+            <label>CITY:
+               <input type='text'   name="city" value={formData.city}  onChange={handleChange} />
+               </label>
             <label>SUBJECT: <input type='text' name="subject"  value={formData.subject}  onChange={handleChange} /></label>
             <label>PHONE: <input type='number'  name="phonenumber"value={formData.phonenumber}  onChange={handleChange} /></label>
             <label>STANDARD: <input type='text'  name="standard" value={formData.standard}  onChange={handleChange} /></label>
