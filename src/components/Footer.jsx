9f246ccc932fcfd61daa9a61722f8ab28324290a
Chaimcv <chaithanyamcv@gmail.com>
2026-07-11 21:43:42 +0530

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
          <div className='m-5 w-[50%]'>
           <ul className='font-semibold list-disc ml-32'>
             <li>Online Admission</li>
-           <li>Cyber Security Awareness </li>  
+            <li>Cyber Security Awareness </li>  
             <li>Right to Information</li> 
           </ul>
          </div>
